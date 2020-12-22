2f0956c3ba5ff1683eb382cfa8ccb090439edb42
danyjoelemc2 <76495479+danyjoelemc2@users.noreply.github.com>
2020-12-22 17:13:58 -0500

    Subiendo módulo 2

---
+++ Modulo2/Ejercicios/PROBLEMA1.py
@@ -0,0 +1,12 @@
+
+
+alt = int(input('Altura específica de la pirámide:'))
+
+
+for piramide in range(1, alt+1):
+    
+    print(' '*(alt-piramide)+'#'*piramide)
+    
+    if alt is str:
+        
+        break
